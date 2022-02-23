e38d7524d582ff80c230915b3fe885ebd7866f1a
Dyego Henrique <88366871+zebodexx@users.noreply.github.com>
2022-02-22 21:03:54 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,110 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
+const articleSchema = {
+    title: String,
+    content: String
+}
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.route("/articles")
+.get(function(req, res){
+    Article.find(function(err, foundArticle){
+        if(err){
+            res.send(err);
+        }else{
+            res.send(foundArticle);
+        }
+    });
+})
+.post(function(req, res){
+    const newArticle = new Article ({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save(function(err){
+        if(!err){
+            res.send("Item adiconado com sucesso!!");
+        }else{
+            res.send(err);
+        }
+    });
+})
+.delete(function(req, res){
+    Article.deleteMany(function(err){
+        if(!err){
+            res.send("A exclusao de todos os itens foram feitas com sucesso!!");
+        }
+        else{
+            res.send(err);
+        }
+    });
+});
+
+app.route("/articles/:articleTitle")
+.get(function(req, res){
+    Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+        if(foundArticle){
+            res.send(foundArticle);
+        }else{
+            res.send("Nao foi encontrado o artigo pesquisado!!");
+        }
+    });
+})
+.put(function(req, res){
+    Article.updateOne(
+        {title: req.params.articleTitle},
+        {title: req.body.title, content: req.body.content},
+        function(err){
+            if(!err){
+                res.send("Dados atualizados com sucesso!!");
+            }else{
+                res.send(err);
+            }
+        }
+    );
+})
+.patch(function(req, res){
+    Article.updateOne(
+        {title: req.params.articleTitle},
+        {$set: req.body},
+        function(err){
+            if(!err){
+                res.send("Atualizacao de dados feita com sucesso!!");
+            }else{
+                res.send(err);
+            }
+        }
+    );
+})
+.delete(function(req, res){
+    Article.deleteOne(
+        {title: req.params.articleTitle},
+        function(err){
+            if(!err){
+                res.send("Foi possivel deletar o item!!");
+            }else{
+                res.send(err);
+            }
+        }
+    )
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
